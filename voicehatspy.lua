ad051d73848db59ba8ace87f4a8f591808767986
Gian! <79874148+GetGian@users.noreply.github.com>
2022-03-11 12:52:47 +0800

    Create voicehatspy.lua

---
+++ voicehatspy.lua
@@ -0,0 +1,6 @@
+getgenv().VoicechatSpySettings = {
+    TrackVoice3d = false,
+    FollowDistance = 0
+}
+
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/GetGian/Scripts/main/voicechat%20spy.lua'))()
